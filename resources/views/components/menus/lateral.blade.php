77f4c02e7f303796ec6c41dc0497caeda014442d
Juan Carlos Lopez <juancar@pcmac.es>
2025-10-30 18:00:54 +0100

    version:2.0

--- resources/views/components/menus/lateral.blade.php
+++ resources/views/components/menus/lateral.blade.php
@@ -1,9 +1,9 @@
 <flux:navlist class="w-48">
-    <flux:navlist.item :href="route('home')" icon="home">Home</flux:navlist.item>
-    <flux:navlist.item href="#" icon="book-open">Libros</flux:navlist.item>
+    <flux:navlist.item :href="route('home')" icon="home" :current="request()->routeIs('home')">Home</flux:navlist.item>
+    <flux:navlist.item :href="route('libros.catalogo')" icon="book-open" :current="request()->routeIs('libros.catalogo')">Libros</flux:navlist.item>
     @auth
-        <flux:navlist.item :href="route('dashboard')" icon="user-circle">Mi cuenta</flux:navlist.item>
+        <flux:navlist.item :href="route('dashboard')" icon="user-circle" :current="request()->routeIs('dashboard')">Mi cuenta</flux:navlist.item>
     @else
-        <flux:navlist.item :href="route('login')" icon="arrow-right-end-on-rectangle">Acceso</flux:navlist.item>
+        <flux:navlist.item :href="route('login')" icon="arrow-right-end-on-rectangle" :current="request()->routeIs('login')">Acceso</flux:navlist.item>
     @endauth
 </flux:navlist>
--- resources/views/components/menus/principal.blade.php
+++ resources/views/components/menus/principal.blade.php
@@ -1,9 +1,9 @@
 <flux:navbar class="-mb-px max-lg:hidden">
-    <flux:navbar.item :href="route('home')" icon="home">Home</flux:navbar.item>
-    <flux:navbar.item :href="route('libros.catalogo')" icon="book-open">Libros</flux:navbar.item>
+    <flux:navbar.item :href="route('home')" icon="home" :current="request()->routeIs('home')">Home</flux:navbar.item>
+    <flux:navbar.item :href="route('libros.catalogo')" icon="book-open" :current="request()->routeIs('libros.catalogo')">Libros</flux:navbar.item>
     @auth
-        <flux:navbar.item :href="route('dashboard')" icon="user-circle">Mi cuenta</flux:navbar.item>
+        <flux:navbar.item :href="route('dashboard')" icon="user-circle" :current="request()->routeIs('dashboard')">Mi cuenta</flux:navbar.item>
     @else
-        <flux:navbar.item :href="route('login')" icon="arrow-right-end-on-rectangle">Acceso</flux:navbar.item>
+        <flux:navbar.item :href="route('login')" icon="arrow-right-end-on-rectangle" :current="request()->routeIs('login')">Acceso</flux:navbar.item>
     @endauth
 </flux:navbar>
